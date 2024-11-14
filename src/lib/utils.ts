1bc837ae6570976dc1b39c5ed5a542d0dc7ecfef
Mohamed Ramadan <mrbp25091993@gmail.com>
2024-11-14 22:26:36 +0200

    First commit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -14,3 +15,34 @@ export const formatePrice = (price: number) => {
 
   return formatter.format(price);
 };
+
+export function constructMetadata({
+  title = "CaseCobra - custom high-quality phone cases",
+  description = "Create custom high-quality phone cases in seconds",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@mramadan32",
+    },
+    icons,
+    metadataBase: new URL("https://casecobramohamedramadan22.vercel.app/"),
+  };
+}
