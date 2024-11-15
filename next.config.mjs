f1098c21adec8b4f7e567b67268e4b69e6b0c915
Mohamed Ramadan <mrbp25091993@gmail.com>
2024-11-15 20:44:17 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,27 @@ const nextConfig = {
   images: {
     domains: ["utfs.io"],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)", // جميع نقاط النهاية تحت /api
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://casecobramohamedramadan22.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS, PUT, DELETE",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
